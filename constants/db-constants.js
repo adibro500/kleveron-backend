2005a4780a01f382f852d16b17d3bd1ed9a52088
Aditya Ammanabrolu <ammanabrolu.aditya@gmail.com>
2018-02-02 09:21:42 +0530

    hello

--- constants/db-constants.js
+++ constants/db-constants.js
@@ -1,4 +1,4 @@
 module.exports={
-// MONGO_URL : "mongodb://root:root@ds133378.mlab.com:33378/kleveron"
-MONGO_URL : "mongodb://localhost:27017/kleveron"
+ MONGO_URL : "mongodb://root:root@ds133378.mlab.com:33378/kleveron"
+//MONGO_URL : "mongodb://localhost:27017/kleveron"
 };
